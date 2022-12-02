087988a10b9232eca306e430ab26d4cda1b05606
Farid Kazim <ferid.kazimli@gmail.com>
2022-12-02 23:04:21 +0400

    delete dist

--- dist/src/configs/serviceList.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const serviceList = {
-    sms: 'http://89.108.99.126/sendsmsapi/'
-};
-exports.default = serviceList;
--- dist/src/modules/users/entity/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserMeta = exports.Users = exports.UserOtp = void 0;
-const UserOtp_1 = require("./UserOtp");
-Object.defineProperty(exports, "UserOtp", { enumerable: true, get: function () { return UserOtp_1.UserOtp; } });
-const Users_1 = require("./Users");
-Object.defineProperty(exports, "Users", { enumerable: true, get: function () { return Users_1.Users; } });
-const UserMeta_1 = require("./UserMeta");
-Object.defineProperty(exports, "UserMeta", { enumerable: true, get: function () { return UserMeta_1.UserMeta; } });
